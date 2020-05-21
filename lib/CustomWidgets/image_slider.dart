09a53568975a0683b2efe7ffe76de02bf9bc8b90
Vikas <nalevikas5@gmail.com>
2020-05-21 12:30:25 +0530

    code optimization

---
+++ lib/CustomWidgets/image_slider.dart
@@ -0,0 +1,34 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+
+class ImageSlider extends StatefulWidget {
+  double height;
+  List<AssetImage> ImageList;
+  ImageSlider(this.height,this.ImageList);
+
+  @override
+  _ImageSliderState createState() => _ImageSliderState();
+}
+
+class _ImageSliderState extends State<ImageSlider> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 19.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        height: widget.height,
+        child: Carousel(
+          borderRadius: true,
+          autoplayDuration: Duration(seconds: 2),
+          showIndicator: false,
+          boxFit: BoxFit.cover,
+          images: widget.ImageList,
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:dmms/CustomWidgets/image_slider.dart';
 import 'package:dmms/Models/dashboard_cards.dart';
 import 'package:dmms/data/data.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<AssetImage> imageList = [
+    AssetImage('assets/car1.jpg'),
+    AssetImage('assets/car2.jpg'),
+    // AssetImage('assets/change-password.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +32,7 @@ class _HomeState extends State<Home> {
           ),
           onPressed: () {},
         ),
-        backgroundColor:Color.fromRGBO(22, 158, 183, 1),
+        backgroundColor: Color.fromRGBO(22, 158, 183, 1),
         title: Text(
           'DMMS Nursing Academy',
           style: TextStyle(
@@ -35,82 +43,46 @@ class _HomeState extends State<Home> {
         ),
       ),
       body: ListView(
-        shrinkWrap: true,
-        // physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          // Container(
-          //   height: 1,
-          //   color: Color.fromRGBO(22, 158, 183, 1),
-          // ),
-          Center(
-            child: Padding(
-              padding: EdgeInsets.all(24.0),
-              child: Text(
-                'WELCOME Vikas',
-                style: TextStyle(
-                  fontFamily: 'nunito_bold',
-                  color: Colors.grey[700],
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 19.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              height: 60,
-              child: Carousel(
-                borderRadius: true,
-                autoplayDuration: Duration(seconds: 2),
-                showIndicator: false,
-                boxFit: BoxFit.cover,
-                images: [
-                  AssetImage('assets/car1.jpg'),
-                  AssetImage('assets/car2.jpg'),
-                  // AssetImage('assets/change-password.png'),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 16),
-          Expanded(
-            child: GridView.count(
-              shrinkWrap: true,
-              physics: ScrollPhysics(),
-              padding: EdgeInsets.all(10),
-              crossAxisCount: 3,
-              children: List.generate(cards.length, (index) {
-                DashCard card = cards[index];
-                return buildCardItem(card);
-              }),
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 18.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              height: 120,
-              child: Carousel(
-                borderRadius: true,
-                autoplayDuration: Duration(seconds: 2),
-                showIndicator: false,
-                boxFit: BoxFit.cover,
-                images: [
-                  AssetImage('assets/car1.jpg'),
-                  AssetImage('assets/car2.jpg'),
-                  // AssetImage('assets/change-password.png'),
-                ],
-              ),
-            ),
-          ),
-        ],
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+      // Container(
+      //   height: 1,
+      //   color: Color.fromRGBO(22, 158, 183, 1),
+      // ),
+      Center(
+      child: Padding(
+          padding: EdgeInsets.all(24.0),
+      child: Text(
+        'WELCOME Vikas',
+        style: TextStyle(
+          fontFamily: 'nunito_bold',
+          color: Colors.grey[700],
+          fontSize: 20,
+        ),
       ),
+    ),
+    ),
+    ImageSlider(60, imageList),
+    SizedBox(height: 16),
+    Expanded(
+    child: GridView.count(
+    shrinkWrap: true,
+    physics: ScrollPhysics(),
+    padding: EdgeInsets.all(10),
+    crossAxisCount: 3,
+    children: List.generate(cards.length, (index) {
+    DashCard card = cards[index];
+    return buildCardItem(card);
+    }),
+    ),
+    ),
+    SizedBox(height: 20),
+    ImageSlider(120, imageList),
+    ]
+    ,
+    )
+    ,
     );
   }
 
