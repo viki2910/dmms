8beceec4de29101d03f0e9955bcdcc865d0a153e
prathmesh16 <40669323+prathmesh16@users.noreply.github.com>
2020-05-30 01:51:16 +0530

    API Added

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -1,14 +1,63 @@
 import 'package:dmms/CustomWidgets/edit_text_style.dart';
+import 'package:dmms/Screens/Login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class RegisterScreen extends StatefulWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
 }
+class RegistrationResult{
+  final String statusCode;
+  final String status;
+  final List<RegistrationData> data;
+  RegistrationResult({this.statusCode,this.status,this.data});
+  factory RegistrationResult.fromJson(Map<String, dynamic> json) {
+    return RegistrationResult(
+        statusCode: json['statuscode'],
+        status: json['status'],
+        data:  (json['data'] as List).map<RegistrationData>((json) => RegistrationData.fromJson(json)).toList()
+    );
+  }
+}
+class RegistrationData{
+  final int status;
+  final String memberID;
+  final String password;
+  final String mobile;
 
+  RegistrationData({this.status,this.memberID,this.password,this.mobile});
+  factory RegistrationData.fromJson(Map<String, dynamic> json) {
+    return RegistrationData(
+        status: json['status'],
+        memberID: json['MEMBERID'],
+        password: json['PASSWORD'],
+        mobile: json['MOBILE']
+    );
+  }
+
+}
+Future<RegistrationResult> fetchData(http.Client client,String name,String password,String email,String mobile,String otp) async {
+  final http.Response response =
+  await http.post('https://www.dmmsmedicalandnursingacademy.com/api/android_service.aspx', headers: <String, String>{
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+  },body:<String,String>{
+    "Request":'{"MethodName":"register","mobile":"${mobile}","name":"${name}","email":"${email}","password":"${password}","otp":"${otp}"}'
+  });
+
+  return RegistrationResult.fromJson(json.decode(response.body));
+}
 class _RegisterScreenState extends State<RegisterScreen> {
+  final name = TextEditingController();
+  final mobile = TextEditingController();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final state = TextEditingController();
+  Future<RegistrationResult> future;
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -42,6 +91,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     SizedBox(height: 20),
                     TextField(
+                      controller: name,
                       decoration: editTextStyle.copyWith(hintText: 'Name').copyWith(
                         prefixIcon: Icon(
                           Icons.account_circle,
@@ -50,6 +100,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     SizedBox(height: 15),
                     TextField(
+                      controller: mobile,
                       decoration: editTextStyle.copyWith(hintText: 'Mobile No.').copyWith(
                         prefixIcon: Icon(Icons.phone_android),
                       ),
@@ -57,6 +108,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     SizedBox(height: 15),
                     TextField(
+                      controller: email,
                       decoration: editTextStyle.copyWith(hintText: 'Email-ID').copyWith(
                         prefixIcon: Icon(
                           Icons.email,
@@ -65,6 +117,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     SizedBox(height: 15),
                     TextField(
+                      controller: password,
                       obscureText: true,
                       decoration: editTextStyle.copyWith(hintText: 'Password').copyWith(
                         prefixIcon: Icon(
@@ -74,6 +127,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     ),
                     SizedBox(height: 15),
                     TextField(
+                      controller: state,
                       decoration: editTextStyle.copyWith(hintText: 'Select State').copyWith(
                         prefixIcon: Icon(
                           Icons.location_city,
@@ -98,7 +152,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          submit();
+                        },
                         child: Padding(
                           padding: EdgeInsets.symmetric(vertical: 12),
                           child: Text(
@@ -137,4 +193,31 @@ class _RegisterScreenState extends State<RegisterScreen> {
       ),
     );
   }
+
+  void submit() async {
+    if(validate())
+      {
+        print(name.text+email.text+mobile.text+password.text);
+        RegistrationResult result =await fetchData(http.Client(), name.text, password.text, email.text, mobile.text, "");
+        if(result.data[0].status==1)
+        {
+          print("success");
+          Navigator.push(context, MaterialPageRoute(
+                              builder: (_)=>LoginScreen()
+                          ));
+        }
+        else{
+          print("failed!");
+        }
+      }
+  }
+
+  bool validate() {
+    if(name.text==""||email.text==""||mobile.text==""||password.text=="")
+      {
+          return false;
+      }
+    return true;
+  }
+
 }
